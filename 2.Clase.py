b00eba60e46dd8020ccbd43995a02db2fc895d00
Jhon Sandoval <jrueda395@gmail.com>
2023-05-18 20:15:16 -0500

    Ejercicio

--- 2.Clase.py
+++ 2.Clase.py
@@ -9,5 +9,37 @@ print(type(edad));
 print(f"mi altura es {altura}")
 print(type(altura));
 
+### DUPLAX CON COLORES DE LA BANDERA ###
+
+##Define una tupla con los colores de tu bandera
+
+colores_Bandera = ('Amarrilo', 'Azul', 'Rojo');
+print(f"Los colores de mi bandera son {colores_Bandera}") 
+print(type(colores_Bandera))
+
+##Lista 
+# #crea una lista con los simbolos patrioticos de tu pais
+
+simbolos = ['bandera', 'escudo', 'himno nacioal']
+print(f"Los simbolos de mi bandera son {simbolos}") 
+print(type(simbolos))
+
+
+##crea un diccionario con datos de tu pais, ejemplo nombre, poblacion, coordenadas, 
+# continente, ciudades principales etc..
+
+###Diccionarios 
+
+datos_Pais = {"pais": "Colombia" ,
+              "poblacion": 51.52, 
+              "coordenadas":  "4.5709° N, 74.2973° W",
+              "ciudades": "Bucaramanga, etc"  
+            }
+
+print(f"el diccioanrio de mi pais son {datos_Pais}") 
+print(type(datos_Pais))
+
+
+
 
 
