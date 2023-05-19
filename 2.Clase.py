0dc0fb8d3d087734a6d55a5c9bae5fd98596e87c
Jhon Sandoval <jrueda395@gmail.com>
2023-05-18 20:18:50 -0500

    corregir

--- 2.Clase.py
+++ 2.Clase.py
@@ -10,24 +10,17 @@ print(f"mi altura es {altura}")
 print(type(altura));
 
 ### DUPLAX CON COLORES DE LA BANDERA ###
-
 ##Define una tupla con los colores de tu bandera
-
 colores_Bandera = ('Amarrilo', 'Azul', 'Rojo');
 print(f"Los colores de mi bandera son {colores_Bandera}") 
 print(type(colores_Bandera))
-
 ##Lista 
 # #crea una lista con los simbolos patrioticos de tu pais
-
 simbolos = ['bandera', 'escudo', 'himno nacioal']
 print(f"Los simbolos de mi bandera son {simbolos}") 
 print(type(simbolos))
-
-
 ##crea un diccionario con datos de tu pais, ejemplo nombre, poblacion, coordenadas, 
 # continente, ciudades principales etc..
-
 ###Diccionarios 
 
 datos_Pais = {"pais": "Colombia" ,
@@ -36,7 +29,7 @@ datos_Pais = {"pais": "Colombia" ,
               "ciudades": "Bucaramanga, etc"  
             }
 
-print(f"el diccioanrio de mi pais son {datos_Pais}") 
+print(f"el diccioanario de mi pais son {datos_Pais}") 
 print(type(datos_Pais))
 
 
